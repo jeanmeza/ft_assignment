0bacefe8ae277734ac22f2fad9dd684c80744a30
Jean Meza <meza.jean@proton.me>
2026-05-31 15:49:29 +0200

    generate assertion for the TestFindAllOwner test

--- src/test/java/com/example/TestFindAllOwner.java
+++ src/test/java/com/example/TestFindAllOwner.java
@@ -15,8 +15,8 @@ public class TestFindAllOwner extends BaseTest {
 		driver.findElement(By.cssSelector("[type='submit']")).click();
 		Thread.sleep(UI_WAIT_MS);
 
-		org.junit.jupiter.api.Assertions.assertTrue(driver.getPageSource().contains("Black"));
-
+		driver.findElement(By.id("owners"));
+		org.junit.jupiter.api.Assertions.assertTrue(driver.findElement(By.id("owners")).isDisplayed());
 	}
 
 }
