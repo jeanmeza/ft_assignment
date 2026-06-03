b64a99e41ff8c2d445e20d4b12b712513aff49d1
Jean Meza <meza.jean@proton.me>
2026-06-03 16:49:27 +0200

    put the "no-comments" tests in its own package

---
+++ src/test/java/com/example/nocomments/TestAddOwnerWithEmptyLastName.java
@@ -0,0 +1,42 @@
+package com.example.nocomments;
+
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.By;
+
+
+public class TestAddOwnerWithEmptyLastName extends BaseTest {
+
+
+	@Test
+	public void TestAddOwnerWithEmptyLastNameM() throws InterruptedException {
+
+		String firstName = "Busra";
+		String lastName = "";
+		String address = "Via torti";
+		String city = "Genova";
+		String telephone = "3381564885";
+
+		// Click find owner button to open find owner screen
+		driver.findElement(By.cssSelector("[title='find owners']")).click();
+		Thread.sleep(UI_WAIT_MS);
+
+		// In find owner screen, click add owner button to add new owner
+		driver.findElement(By.linkText("Add Owner")).click();
+		Thread.sleep(UI_WAIT_MS);
+
+		// Fill owner informations
+		driver.findElement(By.id("firstName")).sendKeys(firstName);
+		driver.findElement(By.id("lastName")).sendKeys(lastName);
+		driver.findElement(By.id("address")).sendKeys(address);
+		driver.findElement(By.id("city")).sendKeys(city);
+		driver.findElement(By.id("telephone")).sendKeys(telephone);
+
+		// Click add owner button to save new owner
+		driver.findElement(By.cssSelector("[type='submit']")).click();
+		Thread.sleep(UI_WAIT_MS);
+
+		org.junit.jupiter.api.Assertions.assertTrue(driver.findElement(By.cssSelector("[type='submit']")).isDisplayed());
+
+	}
+
+}
---
+++ src/test/java/com/example/nocomments/TestAddOwnerWithLongTelephone.java
@@ -0,0 +1,51 @@
+package com.example.nocomments;
+
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.By;
+
+import static org.junit.jupiter.api.Assertions.fail;
+
+public class TestAddOwnerWithLongTelephone extends BaseTest {
+
+
+	@Test
+	public void TestAddOwnerWithLongTelephoneM() throws InterruptedException {
+
+		String firstName = "Busra";
+		String lastName = "Ertugrul";
+		String address = "Via Torti";
+		String city = "Genova";
+		String telephone = "1234567898765";
+
+		// Click find owner button to open find owner screen
+		driver.findElement(By.cssSelector("[title='find owners']")).click();
+		Thread.sleep(UI_WAIT_MS);
+
+		// In find owner screen, click add owner button to add new owner
+		driver.findElement(By.linkText("Add Owner")).click();
+		Thread.sleep(UI_WAIT_MS);
+
+		// Fill owner informations
+		driver.findElement(By.id("firstName")).sendKeys(firstName);
+		driver.findElement(By.id("lastName")).sendKeys(lastName);
+		driver.findElement(By.id("address")).sendKeys(address);
+		driver.findElement(By.id("city")).sendKeys(city);
+		driver.findElement(By.id("telephone")).sendKeys(telephone);
+
+		// Click add owner button to save new owner
+		driver.findElement(By.cssSelector("[type='submit']")).click();
+		Thread.sleep(UI_WAIT_MS);
+
+		try {
+
+			org.junit.jupiter.api.Assertions.assertTrue(driver.findElement(By.cssSelector("[type='submit']")).isDisplayed());
+
+		} catch (Exception e) {
+
+			fail("Numeric value out of bounds (<10 digits>.<0 digits> expected) error message could not found(For Telephone): "
+				+ e.getMessage());
+		}
+
+	}
+
+}
